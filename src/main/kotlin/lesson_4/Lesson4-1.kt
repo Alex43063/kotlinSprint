ae5ff08e5e83cdfc50778f2fad45399a7c8a0511
aleksandr <adavydov@jaxel.com>
2024-09-20 18:04:27 +0300

    KS-4-1 Small fix

--- src/main/kotlin/lesson_4/Lesson4-1.kt
+++ src/main/kotlin/lesson_4/Lesson4-1.kt
@@ -22,5 +22,5 @@ fun main() {
     val bookedTableToday = 13
     val bookedTableYesterday = 9
     println("Доступность столиков на сегодня: ${bookedTableToday < ALL_TABLE}")
-    println("Доступность столиков на сегодня: ${bookedTableYesterday < ALL_TABLE}")
+    println("Доступность столиков на завтра: ${bookedTableYesterday < ALL_TABLE}")
 }
