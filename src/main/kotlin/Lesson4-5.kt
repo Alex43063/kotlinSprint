8e910d375f08ef2f681f8483162824ffe6b431c3
aleksandr <adavydov@jaxel.com>
2024-09-21 16:31:34 +0300

    KS-4-5 Calculate opportunity for starting ship

---
+++ src/main/kotlin/Lesson4-5.kt
@@ -0,0 +1,30 @@
+const val GOOD_CONDITION_SHIP_NOT_BROKEN = false
+const val GOOD_CONDITION_SHIP_IS_BROKEN = true
+const val GOOD_CONDITION_SEAMAN_MIN = 55
+const val GOOD_CONDITION_SEAMAN_MAX = 70
+const val GOOD_CONDITION_FOOD_BOX = 50
+
+fun main() {
+    println("Is ship broken? yes/no")
+    val isBroken = readlnOrNull() == "yes"
+    println("Count of seamen")
+    val numOfSeamen = readlnOrNull()?.toInt()
+    println("Count of boxes with food")
+    val numFoodBox = readlnOrNull()?.toInt()
+    println("Is good weather? yes/no")
+    val isGoodWeather = readlnOrNull() == "yes"
+
+    if (numFoodBox != null) {
+        println(
+            "${
+                ((isBroken == GOOD_CONDITION_SHIP_NOT_BROKEN)
+                        && (numOfSeamen in GOOD_CONDITION_SEAMAN_MIN..GOOD_CONDITION_SEAMAN_MAX)
+                        && (numFoodBox > GOOD_CONDITION_FOOD_BOX)) ||
+                        ((isBroken == GOOD_CONDITION_SHIP_IS_BROKEN) &&
+                                (numOfSeamen == GOOD_CONDITION_SEAMAN_MAX)
+                                && (isGoodWeather) && (numFoodBox >= GOOD_CONDITION_FOOD_BOX))
+            }"
+        )
+    }
+}
+
