cc433f3be2a5142bbe5ad77844d8f0fff119fb0b
aleksandr <adavydov@jaxel.com>
2024-09-22 09:53:42 +0300

    KS-4-5 Small fixes after code review part2

--- src/main/kotlin/Lesson4-5.kt
+++ src/main/kotlin/Lesson4-5.kt
@@ -5,10 +5,13 @@ const val GOOD_CONDITION_FOOD_BOX = 50
 fun main() {
     println("Is ship broken? yes/no")
     val isBroken = readlnOrNull() == "yes"
+
     println("Count of seamen")
     val numOfSeamen = readlnOrNull()?.toInt()
+
     println("Count of boxes with food")
     val numFoodBox = readlnOrNull()?.toInt()
+
     println("Is good weather? yes/no")
     val isGoodWeather = readlnOrNull() == "yes"
 
