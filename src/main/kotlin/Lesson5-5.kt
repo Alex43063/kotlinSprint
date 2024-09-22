dc93b9ba1cad837611f84badd8bcb89d83d9dd86
aleksandr <adavydov@jaxel.com>
2024-09-22 15:57:15 +0300

    KS-5-4 Lottery with 3 numbers

---
+++ src/main/kotlin/Lesson5-5.kt
@@ -0,0 +1,22 @@
+import kotlin.random.Random
+
+fun main() {
+    val listOfLuckyNumbers = listOf(Random.nextInt(43), Random.nextInt(43), Random.nextInt(43))
+    val listWithUserNumbers: MutableList<Int> = mutableListOf()
+
+    println("Введите три числа")
+    for (i in 0..2) {
+        listWithUserNumbers.addLast(readln().toInt())
+    }
+
+    val correctAnswers = listOfLuckyNumbers.intersect(listWithUserNumbers)
+
+    when (correctAnswers.size) {
+        3 -> println("Вы угадали все числа и выиграли джекпот")
+        2 -> println("Вы угадали два числа и получаете крупный приз")
+        1 -> println("Вы угадали одно число и получаете утешительный приз")
+        0 -> println("Вы не угадали ни одного числа")
+    }
+
+    println("Правильный ответы: $listOfLuckyNumbers")
+}
