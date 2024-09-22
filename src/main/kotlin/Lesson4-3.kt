d3ab160a5bb6e87ca5460f30d48ccfa72b24f3d6
aleksandr <adavydov@jaxel.com>
2024-09-22 09:43:45 +0300

    KS-4-3 Small fixes after code review

--- src/main/kotlin/Lesson4-3.kt
+++ src/main/kotlin/Lesson4-3.kt
@@ -1,5 +1,5 @@
-const val GOOD_CONDITIONS_IS_SUNNY_DAY = true
-const val GOOD_CONDITIONS_TENT_IS_OPENED = true
+const val IS_GOOD_CONDITIONS_SUNNY_DAY = true
+const val IS_GOOD_CONDITIONS_TENT_OPENED = true
 const val GOOD_CONDITIONS_AIR_HUMIDITY = 20
 const val NOT_GOOD_CONDITIONS_TYPE_SEASON = "Winter"
 
@@ -10,6 +10,6 @@ fun main() {
     val typeOfSeason = "Winter"
 
     println("Благоприятные ли условия сейчас для роста бобовых? " +
-            "${(isSunnyDay == GOOD_CONDITIONS_IS_SUNNY_DAY) && (isOpenedTent == GOOD_CONDITIONS_TENT_IS_OPENED) && 
+            "${(isSunnyDay == IS_GOOD_CONDITIONS_SUNNY_DAY) && (isOpenedTent == IS_GOOD_CONDITIONS_TENT_OPENED) && 
                     (airHumidity == GOOD_CONDITIONS_AIR_HUMIDITY) && (typeOfSeason != NOT_GOOD_CONDITIONS_TYPE_SEASON)}")
 }
